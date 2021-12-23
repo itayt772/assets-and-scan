ee0158430c26f6422cd8b8effe4121c03f39483b
itayt <itayt@amplicy.io>
2021-12-23 11:52:29 +0200

    review

--- controllers/assetController.js
+++ controllers/assetController.js
@@ -1,54 +1,66 @@
-const Asset = require('../models/assetModel');
-const {catchAsync}=require('../errorHandler');
-const scanJodController = require('./scanJobController');
+const Asset = require("../models/assetModel");
+const { catchAsync } = require("../errorHandler");
+const scanJodController = require("./scanJobController");
 
-module.exports.createAsset = catchAsync(async (req, res,next) => {
+/**
+ * General notes:
+ * - Move logic from controller to servies.
+ * - Always prefer using "const" over "let" if you can.
+ */
 
-    let newAsset = await Asset.create(req.body);
-    res.status(201).json({
-        status: 'success',
-        newDoc: newAsset
-    })
-
+module.exports.createAsset = catchAsync(async (req, res, next) => {
+  // move logic to service (assetService.create())
+  let newAsset = await Asset.create(req.body);
+  // Use interface for response model ({ status: 'success' | 'fail', data<T>}) for example
+  res.status(201).json({
+    status: "success",
+    newDoc: newAsset,
+  });
 });
 
+module.exports.getAll = catchAsync(async (req, res, next) => {
+  let perPage = req.query.perPage * 1 || 5;
+  let page = req.query.page * 1 || 1;
 
-module.exports.getAll = catchAsync(async (req, res,next) => {
+  let assets = await Asset.find()
+    .skip(perPage * page - perPage)
+    .limit(perPage);
 
-    let perPage = req.query.perPage * 1 || 5;
-    let page = req.query.page * 1 || 1;
-
-    let assets = await Asset.find().skip(perPage * page - perPage).limit(perPage);
-
-    if (req.query.page) {
-        const documentsNum = await Asset.countDocuments();
-        if (perPage * page - perPage >= documentsNum){
-            throw new Error('This page doesnt exist')
-        }
+  if (req.query.page) {
+    // In mongo you can use $facet for example to get assets and counts for pagination in one query to mongo.
+    // Return error in that case is useless, you can just return empty array
+    const documentsNum = await Asset.countDocuments();
+    if (perPage * page - perPage >= documentsNum) {
+      throw new Error("This page doesnt exist");
     }
+  }
 
-    res.status(200).json({
-        status: 'success',
-        assets
-    })
+  // Using pagniation you will have to return the total count number so client side can implement pagination based on this info.
+  res.status(200).json({
+    status: "success",
+    assets,
+  });
 });
 
-module.exports.getAssetById = catchAsync(async (req, res,next) => {
-
-    let asset = await Asset.findById(req.params.id);
-    let scans = await scanJodController.findScanJobsByAssetId(req.params.id);
-    res.status(200).json({
-        status: 'success',
-        asset,
-        scans
-    })
+module.exports.getAssetById = catchAsync(async (req, res, next) => {
+  let asset = await Asset.findById(req.params.id);
+  let scans = await scanJodController.findScanJobsByAssetId(req.params.id);
+  res.status(200).json({
+    status: "success",
+    asset,
+    scans,
+  });
 });
 
-module.exports.getAssetByName = catchAsync(async (req, res,next) => {
-
-    let assets = await Asset.find({ "name": req.query.name });
-    res.status(200).json({
-        status: 'success',
-        assets
-    })
+/**
+ * - getAssetsByName - and you can remove this function:
+ * - You already implement pagination in 'getAll' - what if we use this function and have many assets?
+ * - You should use 'getAll' with filters and then implement pagination
+ */
+module.exports.getAssetByName = catchAsync(async (req, res, next) => {
+  let assets = await Asset.find({ name: req.query.name });
+  res.status(200).json({
+    status: "success",
+    assets,
+  });
 });
--- controllers/scanJobController.js
+++ controllers/scanJobController.js
@@ -1,47 +1,54 @@
-const mongoose = require('mongoose');
-const ScanJob = require('../models/scanJobModel');
-const Queue = require('bull');
-const {catchAsync}=require('../errorHandler');
+const mongoose = require("mongoose");
+const ScanJob = require("../models/scanJobModel");
+const Queue = require("bull");
+const { catchAsync } = require("../errorHandler");
 
-const jobQueue = new Queue('jobScanQueue', 'redis://127.0.0.1:6379')
+// Queue logic should be in another file "job-queue.js"
+const jobQueue = new Queue("jobScanQueue", "redis://127.0.0.1:6379");
 
+module.exports.createScan = catchAsync(async (req, res, next) => {
+  let currDate = new Date();
+  let scanDueDate = new Date(currDate.getTime() + req.body.minutes * 60000);
 
-module.exports.createScan = catchAsync(async (req, res,next) => {
+  // If I understand you are using the value of status from the request body, what if I inject "fail" in my body?
+  // The default value of new scan should be always "pending"
+  let newScan = await ScanJob.create({
+    status: req.body.status,
+    scanDueDate,
+    asset: req.body.asset,
+  });
 
-    let currDate = new Date();
-    let scanDueDate = new Date(currDate.getTime() + req.body.minutes * 60000);
+  await jobQueue.add(
+    { scanId: newScan._id },
+    { delay: req.body.minutes * 60000 }
+  );
 
-    let newScan = await ScanJob.create({
-        status: req.body.status,
-        scanDueDate,
-        asset: req.body.asset
-    });
-
-    await jobQueue.add({ scanId: newScan._id }, { delay: req.body.minutes * 60000 });
-
-    res.status(201).json({
-        status: 'success',
-        newDoc: newScan
-    })
+  res.status(201).json({
+    status: "success",
+    newDoc: newScan,
+  });
 });
 
+// Same here, move to "assetService.findScansJobByAssetId(assetId)"
 module.exports.findScanJobsByAssetId = async function (id) {
-    return await ScanJob.find({ "asset": mongoose.Types.ObjectId(id) });
-    
-}
+  return await ScanJob.find({ asset: mongoose.Types.ObjectId(id) });
+};
 
-jobQueue.on('completed',async function(job){
-    await ScanJob.findByIdAndUpdate(
-        { _id: job.data.scanId },
-        { status: "succeeded", dateCompleted: new Date()});
-})
+jobQueue.on("completed", async function (job) {
+  await ScanJob.findByIdAndUpdate(
+    { _id: job.data.scanId },
+    // I always suggest never use "hard-coded" strings and prefer enums / Object.freeze
+    { status: "succeeded", dateCompleted: new Date() }
+  );
+});
 
-jobQueue.on('failed',async function(job){
-    await ScanJob.findByIdAndUpdate(
-        { _id: job.data.scanId },
-        { status: "failed", dateCompleted: new Date() });
-})
- 
-jobQueue.process(function(){
-    console.log('job running')
-})
+jobQueue.on("failed", async function (job) {
+  await ScanJob.findByIdAndUpdate(
+    { _id: job.data.scanId },
+    { status: "failed", dateCompleted: new Date() }
+  );
+});
+
+jobQueue.process(function () {
+  console.log("job running");
+});
